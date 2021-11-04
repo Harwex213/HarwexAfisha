f28460e1bb9479b131c1e2f542134d6a31063f16
Harwex213 <harwex213@mail.ru>
2021-11-04 22:36:48 +0300

    fix(routesRegister): true error message of unsupported method

--- src/app/routesRegister.js
+++ src/app/routesRegister.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
     app.use((request, response, next) => {
-        const error = new Error("Unhandled method");
+        const error = new Error("Unsupported method");
         error.status = 400;
         error.stack = null;
         next(error);
