97d2a9726898adfae08ccc87d4fc1bfd45ec80f5
Harwex213 <harwex213@mail.ru>
2021-11-04 23:37:32 +0300

    feat(routesRegister): register all controllers

--- src/app/routesRegister.js
+++ src/app/routesRegister.js
@@ -1,7 +1,19 @@
-const cityRoutes = require("../controllers/citiesController");
+const citiesRoutes = require("../controllers/citiesController");
+const eventsRoutes = require("../controllers/eventsController");
+const placesRoutes = require("../controllers/placesController");
+const eventPlacesRoutes = require("../controllers/eventPlacesController");
+const sessionsRoutes = require("../controllers/sessionsController");
+const usersRoutes = require("../controllers/usersController");
+const ticketsRoutes = require("../controllers/ticketsController");
 
 module.exports = (app) => {
-    app.use("/city", cityRoutes);
+    app.use("/api/cities", citiesRoutes);
+    app.use("/api/events", eventsRoutes);
+    app.use("/api/places", placesRoutes);
+    app.use("/api/eventPlaces", eventPlacesRoutes);
+    app.use("/api/sessions", sessionsRoutes);
+    app.use("/api/users", usersRoutes);
+    app.use("/api/tickets", ticketsRoutes);
 
     app.use((request, response, next) => {
         const error = new Error("Unsupported method");
